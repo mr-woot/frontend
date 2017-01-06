d6b1716fa137ce783daa53f7b96bf210270640f9
mr-woot <tusharmudgal3@gmail.com>
2017-01-06 12:48:50 +0530

    fixes

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-angular.module('app', ['satellizer']);
+// Closure for best practices.
+
+(function() {
+    // For Angular Setup.
+    // var app = angular.module('app', []);
+
+    // Scripts goes here.
+
+
+})();
